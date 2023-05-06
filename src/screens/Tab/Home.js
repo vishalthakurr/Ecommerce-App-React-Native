b62c9b0da9377c4e693ed8df7144b03075fa436c
vishalthakurr <vishalthakurr302@gmail.com>
2023-05-06 15:43:39 +0530

    call api and product list

--- src/screens/Tab/Home.js
+++ src/screens/Tab/Home.js
@@ -1,10 +1,34 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  FlatList,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import Header from '../../common/Header';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation();
+  const [products, setproducts] = useState([]);
+
+
+  //*api call get product
+  const getproduct = async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    let data = await res.json();
+    if (data.length !== 0) {
+      setproducts(data);
+    } else {
+      // Something went wrong
+    }
+  };
+  useEffect(() => {
+    getproduct();
+  }, []);
+
   return (
     <View View style={styles.conatiner}>
       <Header
@@ -15,6 +39,33 @@ const Home = () => {
           navigation.openDrawer();
         }}
       />
+      {/* Products list */}
+
+      <FlatList
+        style={styles.ListProduct}
+        data={products}
+        renderItem={({item, index}) => {
+          return (
+            <View style={styles.ProductItem}>
+              <Image source={{uri: item.image}} style={styles.ItemImage} />
+              <View>
+                <Text style={styles.name}>
+                  {item.title.length >25
+                    ? item.title.substring(0, 25) + '...'
+                    : item.title}
+                </Text>
+
+                <Text style={styles.desc}>
+                  {item.description.length > 30
+                    ? item.description.substring(0, 30) + '...'
+                    : item.description}
+                </Text>
+                <Text style={styles.price}> {'$' + item.price}</Text>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
@@ -22,5 +73,35 @@ export default Home;
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
+  },
+  ListProduct: {marginBottom: 60},
+  ProductItem: {
+    width: Dimensions.get('window').width,
+    height: 100,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    margin: 8,
+    alignItems: 'center',
+  },
+  ItemImage: {
+    width: 100,
+    height: 100,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginLeft: 20,
+  },
+  desc: {
+    fontSize: 15,
+    fontWeight: '400',
+    marginLeft: 20,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginLeft: 20,
+    color: 'green',
+    marginTop: 5,
   },
 });
